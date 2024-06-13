03122dc2765c8859e53da286bad31b272802fdb2
Cassandra Keddis <cassandra@thinkific.com>
2024-06-12 20:34:06 -0500

    step1: create button disabled before input

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,32 @@
+import React, { useState } from 'react';
 import styles from "./header.module.css";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { uppercase } from "../../helpers/stringHelpers";
 
 export function Header() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value.trimStart());
+  };
+
+  const isButtonDisabled = inputValue.trim() === "";
+
   return (
     <header className={styles.header}>
-      {/* This is simply to show you how to use helper functions */}
       <h1>{uppercase("bcit")} Assignment Tracker</h1>
       <form className={styles.newAssignmentForm}>
-        <input placeholder="Add a new assignment" type="text" />
-        <button>
+        <input
+          placeholder="Add a new assignment"
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button
+          type="button"
+          disabled={isButtonDisabled}
+          className={isButtonDisabled ? styles.disabledButton : ""}
+        >
           Create <AiOutlinePlusCircle size={20} />
         </button>
       </form>
